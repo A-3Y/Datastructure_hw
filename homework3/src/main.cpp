060590e9b4541b6506308c347fbb4b561fdaed25
You-Yu-Yang <41143245@nfu.edu.tw>
2025-08-09 15:13:42 +0800

    Add files via upload

---
+++ homework3/src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Polynomial.h"
+using namespace std;
+
+int main() {
+    Polynomial p, q, r;
+    cout << "Enter polynomial p (n c1 e1 c2 e2 ...): ";
+    cin >> p;
+    cout << "Enter polynomial q (n c1 e1 c2 e2 ...): ";
+    cin >> q;
+
+    cout << "p = " << p << endl;
+    cout << "q = " << q << endl;
+
+    r = p + q;
+    cout << "p + q = " << r << endl;
+
+    r = p - q;
+    cout << "p - q = " << r << endl;
+
+    r = p * q;
+    cout << "p * q = " << r << endl;
+
+    float x;
+    cout << "Enter value to evaluate p(x), q(x): ";
+    cin >> x;
+    cout << "p(" << x << ") = " << p.Evaluate(x) << endl;
+    cout << "q(" << x << ") = " << q.Evaluate(x) << endl;
+
+    return 0;
+}
